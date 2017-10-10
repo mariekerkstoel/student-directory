2724a0a0ad9f7e1f9fe091dc7d3c21d812c9671b
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-10 17:04:36 +0100

    refactored code (step 10)

--- directory.rb
+++ directory.rb
@@ -1,5 +1,5 @@
 
-
+@students = []
 def input_user
 
     puts "Please enter the name of a student"
@@ -42,12 +42,11 @@ def input_user
         end
       end
 
-  students = []
 
   while name != '' do
-    students << {name: name, cohort: cohort.downcase.to_sym, hobbies: hobbies, date: date_of_birth, height: height}
-    puts "now we have #{students.count} student" if (students.count) == 1
-    puts "now we have #{students.count} students" if students.count > 1
+    @students << {name: name, cohort: cohort.downcase.to_sym, hobbies: hobbies, date: date_of_birth, height: height}
+    puts "now we have #{@students.count} student" if (@students.count) == 1
+    puts "now we have #{@students.count} students" if @students.count > 1
     puts "Is there any other students?"
     name = gets.chomp
     if name == ''
@@ -89,7 +88,7 @@ def input_user
     end
 
   end
-    students
+    @students
 end
 
 def print_header
@@ -98,14 +97,14 @@ def print_header
   puts  ("-----------").center(line_width)
 end
 
-def printing_by_cohort(students)
+def printing_by_cohort
   line_width = 100
   @student_count = 0
-  if students.length >= 1
+  if @students.length >= 1
   cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
   cohorts.each{|x|
     puts ("These are the students of the " + x.to_s + " cohort: ").center(line_width)
-    students_cohort = students.select{|hash| hash[:cohort] == x }
+    students_cohort = @students.select{|hash| hash[:cohort] == x }
     students_cohort.each{|name|
       puts  (name[:name].capitalize + ", " + name[:cohort].to_s + " cohort").center(line_width)
       puts  ("This person's hobbies are: " + name[:hobbies] + " ").center(line_width)
@@ -119,23 +118,24 @@ def printing_by_cohort(students)
 end
 
 
-def printing(students)
+def printing
   @student_count = 0
-  while students.length != 0 do
-  name = students.pop
+  while @students.length != 0 do
+  name = @students.pop
   line_width = 100
   puts (name[:name].capitalize + ", " + name[:cohort].to_s + " cohort").center(line_width)
-  puts  ("This person's hobbies are: " + name[:hobbies] + " ").center(line_width)
-  puts  ("Date of birth = " + name[:date] + " and they are " + name[:height] + " tall. ").center(line_width)
+  # uncover this code if you'd like to show this person's extra information
+  #puts  ("This person's hobbies are: " + name[:hobbies] + " ").center(line_width)
+  #puts  ("Date of birth = " + name[:date] + " and they are " + name[:height] + " tall. ").center(line_width)
   puts  ("------------------").center(line_width)
   @student_count += 1
-  if students.length == 0
+  if @students.length == 0
     break
   end
 end
 end
 
-def print_footer(students)
+def print_footer
   if @student_count == 1
       puts ("Overall, we have #{@student_count} great student.").center(100)
   elsif @student_count > 1
@@ -144,30 +144,43 @@ def print_footer(students)
 
 end
 
-def interactive_menu
-  students = []
-  loop do
-    list = ["Input the students", "Show the students", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "Exit"]
-      list.each_with_index{|x,index|
-      puts "#{index + 1}. #{x}" }
-    puts "What would you like to do? "
-    selection = gets.chomp
 
-    case selection
+def print_menu
+  list = ["Input the students", "Show the students", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "Exit"]
+    list.each_with_index{|x,index|
+    puts "#{index + 1}. #{x}" }
+
+end
+
+def show_students
+  print_header
+  printing
+  print_footer
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_user
+      input_user
     when "2"
-      print_header
-      printing_by_cohort(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant."
-    end
+  end
+end
 
-    end
 
+
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    puts "What would you like to do? "
+    process(gets.chomp)
+end
 end
 
 
